8a2f41ba5da0342014b1085ece497f5f6f42ba25
py563 <prajwal@vardhaman.org>
2023-05-12 15:34:24 +0530

    added code to display form

--- todo-app/app.js
+++ todo-app/app.js
@@ -8,11 +8,13 @@ app.use(bodyParser.json());
 app.set("view engine", "ejs");
 
 app.get("/", async (request, response) => {
-  const allTodos = await Todo.getAllTodos();
+  const overdueTodos = await Todo.overdue();
+  const dueTodayTodos = await Todo.dueToday();
+  const dueLaterTodos = await Todo.dueLater();
   if (request.accepts("html")) {
-    response.render("index", { allTodos });
+    response.render("index", { overdueTodos, dueTodayTodos, dueLaterTodos });
   } else {
-    response.json({ allTodos });
+    response.json({ overdueTodos, dueTodayTodos, dueLaterTodos });
   }
 });
 
